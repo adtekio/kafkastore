c93f26a153be2df87317ad624a0e9a7d532114b4
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-06-10 13:47:05 +0200

    testing the redis and inserter
    On branch: more_tests

--- test/functional/authentication_test.rb
+++ test/functional/authentication_test.rb
@@ -12,7 +12,20 @@ class AuthenticaionTest < Minitest::Test
   end
 
   context "authentication" do
-    should "prevent access"
-    should "allow access"
+    should "prevent access" do
+      ["/sidekiq"
+       ].each do |path|
+        get path
+        assert_redirect_to("auth/google_auth2", "Failed for #{path}")
+      end
+    end
+
+    should "allow access" do
+      ["/pingdom"].
+        each do |path|
+        get path
+        assert last_response.ok?, "Failed for #{path}"
+      end
+    end
   end
 end
